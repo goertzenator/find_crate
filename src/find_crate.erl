b5acb9aa3075a1475bc8a974e33c0e80704e340f
Daniel Goertzen <daniel.goertzen@gmail.com>
2017-03-07 09:43:18 -0600

    add shorter functions for default crate artifact

--- src/find_crate.erl
+++ src/find_crate.erl
@@ -1,6 +1,12 @@
 -module(find_crate).
 
 -export([find_library/3, find_executable/3]).
+-export([find_library/2, find_executable/2]).
+
+%% @doc Find default Rust shared library in application priv dir that was built by rust.mk or rebar3 Rust plugin.
+-spec find_library(App::atom(), CrateName::string()) ->
+	{ok, Lib::string()} | {error, Reason::atom()}.
+find_library(App, CrateName) -> find_library(App, CrateName, CrateName).
 
 %% @doc Find Rust shared library in application priv dir that was built by rust.mk or rebar3 Rust plugin.
 -spec find_library(App::atom(), CrateName::string(), LibName::string()) ->
@@ -13,6 +19,11 @@ find_library(App, CrateName, LibName) ->
 		[] -> {error, not_found};
 		_ -> {error, multiple_matches}
 	end.
+
+%% @doc Find default Rust executable in application priv dir that was built by rust.mk or rebar3 Rust plugin.
+-spec find_executable(App::atom(), CrateName::string()) ->
+	{ok, Exe::string()} | {error, Reason::atom()}.
+find_executable(App, CrateName) ->find_executable(App, CrateName, CrateName).
 
 %% @doc Find Rust executable in application priv dir that was built by rust.mk or rebar3 Rust plugin.
 -spec find_executable(App::atom(), CrateName::string(), LibName::string()) ->
